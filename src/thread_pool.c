03cf069f35e987d9acdb1b2074b2e4890b19ec84
Kelby Madal-Hellmuth <kelby@pop-os.localdomain>
2020-11-21 17:56:48 -0800

    Initial work_queue implementation

--- src/thread_pool.c
+++ src/thread_pool.c
@@ -7,18 +7,146 @@
 
 
 typedef struct {
-    int fd;
+    void *arg;
+    int (*func)(void*);
     work_item_t* next;
 } work_item_t;
 
 
+typedef struct {
+    work_item_t* front;
+    work_item_t* back;
+    size_t size;
+    pthread_mutex_t mtx;
+} work_queue_t;
+
+
 struct thread_pool {
-    work_item_t *queue;
-    size_t queue_size;
-    pthread_mutex_t queue_mtx;
+    work_queue_t *queue;
     pthread_t *pool;
     size_t pool_size;
 };
+
+
+static work_queue_t* work_queue_init();
+static void work_queue_deinit(work_queue_t*);
+static void work_queue_enqueue(work_queue_t*, work_item_t*);
+static work_item_t* work_queue_dequeue(work_queue_t*);
+static work_item_t* work_queue_front(work_queue_t*);
+static work_item_t* work_queue_back(work_queue_t*);
+static size_t work_queue_size(work_queue_t*);
+static void fill_pool(pthread_t*, size_t, void* (*)(void*), void*);
+static void* pool_func(void*);
+
+
+static work_queue_t* work_queue_init()
+{
+    work_queue_t *queue = (work_queue_t*)malloc(sizeof(work_queue_t));
+
+    queue->front = NULL;
+    queue->back = NULL;
+    queue->size = 0;
+
+    pthread_mutexattr_t mtx_attr;
+
+    pthread_mutexattr_init(&mtx_attr);
+    pthread_mutexattr_settype(&mtx_attr, PTHREAD_MUTEX_NORMAL);
+
+    pthread_mutex_init(&queue->mtx, &mtx_attr);
+
+    pthread_mutexattr_destroy(&mtx_attr);
+}
+
+
+static void work_queue_deinit(work_queue_t *queue)
+{
+    /* Free queue linked list */
+    if (queue->size > 0) {
+        work_item_t *p, *next;
+        
+        p = queue->front;
+
+        while (p) {
+            next = p->next;
+            free(p);
+            p = next;
+        }
+    }
+
+    pthread_mutex_destroy(&queue->mtx);
+
+    free(queue);
+}
+
+
+static void work_queue_enqueue(work_queue_t *queue, work_item_t *item)
+{
+    if (queue->front == NULL) {
+        queue->front = item;
+        queue->back = item;
+        queue->size = 1;
+    } else {
+        queue->back->next = item;
+        item->next = NULL;
+        queue->back = item;
+        queue->size++;
+    }
+}
+
+
+static work_item_t* work_queue_dequeue(work_queue_t *queue)
+{
+    /* Return NULL when queue is empty */
+    if (queue->size == 0) {
+        return NULL;
+    }
+
+    work_item_t* item = queue->front;
+
+    if (queue->front == queue->back) {
+        /* Case when queue size is 1 */
+        queue->front == NULL;
+        queue->back == NULL;
+    } else {
+        /* Generic case */
+        queue->front = queue->front->next;        
+    }
+
+    queue->size--;
+    return item;
+}
+
+
+static work_item_t* work_queue_front(work_queue_t *queue)
+{
+    return queue->front;
+}
+
+
+static work_item_t* work_queue_back(work_queue_t *queue)
+{
+    return queue->back;
+}
+
+
+static size_t work_queue_size(work_queue_t *queue)
+{
+    return queue->size;
+}
+
+
+static void fill_pool(pthread_t* pool, size_t pool_size, void* (*func)(void*), void* arg)
+{
+    for (int i = 0; i < pool_size; i++) {
+        pthread_create(pool[0], NULL, func, arg);
+    }
+}
+
+
+static void* pool_func(void* arg)
+{
+    return NULL;
+}
 
 
 thread_pool_t* thread_pool_init(int pool_size)
@@ -29,29 +157,20 @@ thread_pool_t* thread_pool_init(int pool_size)
 
     thread_pool_t *tpool = (thread_pool_t*)malloc(sizeof(thread_pool_t));
 
-    tpool->queue = NULL;
-    tpool->queue_size = 0;
+    tpool->queue = work_queue_init();
 
     tpool->pool = (pthread_t*)malloc(sizeof(pthread_t) * pool_size);
     tpool->pool_size = pool_size;
 
-    pthread_mutexattr_t mtx_attr;
-
-    pthread_mutexattr_init(&mtx_attr);
-    pthread_mutexattr_settype(&mtx_attr, PTHREAD_MUTEX_NORMAL);
-
-    pthread_mutex_init(&tpool->queue_mtx, &mtx_attr);
-
-    pthread_mutexattr_destroy(&mtx_attr);
+    fill_pool(tpool->pool, tpool->pool_size, pool_func, NULL);
 }
 
 
 void thread_pool_deinit(thread_pool_t* tpool)
 {
-    /* TODO - free queue linked list*/
-    free(tpool->pool);
+    work_queue_deinit(tpool->queue);
 
-    pthread_mutex_destroy(&tpool->queue_mtx);
+    free(tpool->pool);
 
     free(tpool);
 }
